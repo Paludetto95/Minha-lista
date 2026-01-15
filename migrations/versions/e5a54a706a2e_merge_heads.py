e24f87125734d7d5c003b69d566258f4979e1677
paludetto95 <paludetto12@gmail.com>
2026-01-15 17:27:53 -0300

    correção flask

---
+++ migrations/versions/e5a54a706a2e_merge_heads.py
@@ -0,0 +1,24 @@
+"""Merge heads 5e31e9eff9a2 and d3a7b2e1c9f0
+
+Revision ID: e5a54a706a2e
+Revises: 5e31e9eff9a2, d3a7b2e1c9f0
+Create Date: 2026-01-15 12:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e5a54a706a2e'
+down_revision = ('5e31e9eff9a2', 'd3a7b2e1c9f0')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
